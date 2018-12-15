9fa6e4cc26d079d88ea58f7c4fa6f8179ef31f80
ChimpanG <41950254+chimpang@users.noreply.github.com>
2018-12-15 21:12:21 +0000

    Phase 2 - Arvad CS added.

---
+++ CIVITAS-CSE/CSE City-States Arvad/Core/CSE_Arvad_Localisation.sql
@@ -0,0 +1,28 @@
+/*
+	Localisation
+	Authors: thecrazyscotsman, ChimpanG
+*/
+
+INSERT OR REPLACE INTO LocalizedText (Language, Tag, Text)
+VALUES
+
+-----------------------------------------------
+-- Çatalhöyük
+-----------------------------------------------
+	
+	("en_US",	"LOC_CIVILIZATION_CSE_ARVAD_NAME",			"Arvad"		),
+	("en_US",	"LOC_CIVILIZATION_CSE_ARVAD_ADJECTIVE",		"Arvadite"	),
+	("en_US",	"LOC_CIVILIZATION_CSE_ARVAD_DESCRIPTION",	"{LOC_CIVILIZATION_CSE_ARVAD_NAME} city-state"		),
+	("en_US",	"LOC_TRAIT_CSE_ARVAD_NAME",					"{LOC_CIVILIZATION_CSE_ARVAD_NAME} Suzerain Bonus"	),
+	("en_US",	"LOC_TRAIT_CSE_ARVAD_DESCRIPTION",		
+	"Your City Centers gain +1 [ICON_FOOD] Food and +1 [ICON_PRODUCTION] Production for each adjacent Coast tile."	),
+
+	("en_US",	"LOC_CITY_NAME_AMPI",						"{LOC_CIVILIZATION_CSE_ARVAD_NAME}"	),
+	("en_US",	"LOC_CITY_NAME_KONYA",						"Konya"	),
+
+	("en_US",	"LOC_PEDIA_CITYSTATES_PAGE_CIVILIZATION_CSE_ARVAD_CHAPTER_HISTORY_PARA_1",
+	"Çatalhöyük was a large Neolithic and Chalcolithic proto-city settlement in southern Anatolia. It existed between 7500 BCE to 5700 BCE, flourishing circa 7000 BCE. The site overlooks the Konya Plain, southeast of ancient Iconium."	),
+	("en_US",	"LOC_PEDIA_CITYSTATES_PAGE_CIVILIZATION_CSE_ARVAD_CHAPTER_HISTORY_PARA_2",
+	"Çatalhöyük is composed entirely of domestic buildings with no obvious public buildings. Some rooms have ornate murals, but the purpose of some rooms remains unclear. The people of Çatalhöyük buried their dead within the village, and human remains have been found in many places. There were no apparent social classes and little apparent social distinction based on gender. Upper levels of the cite reveal that the people were gaining agricultural skills. Peas were grown, and almonds, pistachios, and fruits were harvested nearby."	),
+	("en_US",	"LOC_PEDIA_CITYSTATES_PAGE_CIVILIZATION_CSE_ARVAD_CHAPTER_HISTORY_PARA_3",
+	"Çatalhöyük was first excavated by the late, controversial British archaeologist James Mellaart in 1958. After the Dorak affair (in which Mellaart published drawings of supposedly important Bronze Age artifacts that later went missing), he was banned from Turkey. The site lay idle until 1993, when further investigations began under Ian Hodder of the University of Cambridge. Hodder was a former student of Mellaart, and he used the site as his first real-world test of post-processual archaeology."	);
